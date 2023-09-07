1d8301c1f2607d8fe8cc47f2aeb1d33876b2d143
kiruthigarajendran1903 <kiruthiga2807@gmail.com>
2023-09-07 18:16:15 +0530

    practice project

---
+++ PracticeProject9/PracticeProject9/Data/SubjectDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PracticeProject9.Models;
+
+namespace PracticeProject9.Data
+{
+    public class SubjectDbContext : DbContext
+    {
+        public SubjectDbContext (DbContextOptions<SubjectDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PracticeProject9.Models.Subject> Subject { get; set; } = default!;
+    }
+}
---
+++ PracticeProject9/PracticeProject9/Models/Subject.cs
@@ -0,0 +1,17 @@
+﻿namespace PracticeProject9.Models
+{
+    
+        public class Subject
+        {
+            public int Id { get; set; }
+            public int StudentId { get; set; }
+            public string StudentName { get; set; }
+
+            public int SubjectId { get; set; }
+            public string SubjectName { get; set; }
+
+            public int SubjectMark { get; set; }
+        }
+    }
+
+
